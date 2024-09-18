8aa289cca798363e58d7b6f667ad9cef0729d7f2
winy zanin <42894649+WinyZanin@users.noreply.github.com>
2024-09-18 19:22:19 -0400

    Definição de variáveis
    -Definição de portas utilizadas no projeto;
    -Alterado funcionalidade dos reles (HIGH= rele desligado);

--- reator-software.ino
+++ reator-software.ino
@@ -1,5 +1,13 @@
 // arduino mega 2560
 
+/*
+falta:
+- debug pelo SD Card
+- definir pinagem dos sensores
+- criar rotinas de debug
+- fazer função de testes de reles e sensores
+*/
+
 /* relação tempo(minutos) x estágio x atuadores(ligados/desligados)
 *
 * |-------------|-----------|-------|-------|-------|-------|---------------|
@@ -16,28 +24,30 @@
 */
 
 //configuração de pinagem dos Reles
-#define VAP1 22      // Válvula de acionamento pneumático - ENTRADA de efluentes brutos
-#define VAP1_SEL 23  // Seletor de acionamento pneumático - ENTRADA de efluentes brutos
-#define VAP2 24      // Válvula de acionamento pneumático - SAIDA de efluentes tratados
-#define VAP2_SEL 25  // Seletor de acionamento pneumático - SAIDA de efluentes tratados
-#define PUMP 26      // Bomba de ENTRADA de efluentes brutos
-#define VS 28        // valvula solenoide de entrada de ar comprimido
+#define VAP1 38      // Válvula de acionamento pneumático - ENTRADA de efluentes brutos
+#define VAP1_SEL 39  // Seletor de acionamento pneumático - ENTRADA de efluentes brutos
+#define VAP2 40      // Válvula de acionamento pneumático - SAIDA de efluentes tratados
+#define VAP2_SEL 41  // Seletor de acionamento pneumático - SAIDA de efluentes tratados
+#define PUMP 42      // Bomba de ENTRADA de efluentes brutos
+#define VS 43        // valvula solenoide de entrada de ar comprimido
 
 //configuração de pinagem dos Sensores
-#define OD 2           //sensor de oxigênio dissolvido
-#define PH 3           //sensor de pH
-#define NIVEL 4        //sensor de nível do reator
+#define OD A1          //sensor de oxigênio dissolvido
+#define PH A2          //sensor de pH
 #define TEMPERATURA 5  //sensor de temperatura do reator
+#define NIVEL 4        //sensor de nível do reator
+//#define REDOX 6          //sensor de potencial redox
+//#define CONDUTIVIDADE 7  //sensor de condutividade
 
-//variáveis
+//variáveis de tempo
 #define TIME_STAGE_0 10000  //tempo do estagio 0 (enchimento)
 #define TIME_STAGE_1 10000  //tempo do estagio 1 (anaerobico)
 #define TIME_STAGE_2 10000  //tempo do estagio 2 (reação)
 #define TIME_STAGE_3 10000  //tempo do estagio 3 (sedimentação)
 #define TIME_STAGE_4 10000  //tempo do estagio 4 (esvaizamento/retirada)
 #define TIME_STAGE_5 10000  //tempo do estagio 5
-#define TIME_VAP1 10000     //tempo de acionamento da valvula 1 de entrada (VAP1)
-#define TIME_VAP2 10000     //tempo de acionamento da valvula 2 de saída (VAP2)
+#define TIME_VAP1 15000     //tempo de acionamento da valvula 1 de entrada (VAP1)
+#define TIME_VAP2 15000     //tempo de acionamento da valvula 2 de saída (VAP2)
 
 int stage = 0;  //estágio de funcionamento do reator
 
@@ -63,13 +73,13 @@ void setup() {
   pinMode(NIVEL, INPUT);
   pinMode(TEMPERATURA, INPUT);
 
-  //inicialização dos pinos de saída, estagio 0
-  digitalWrite(VAP1, LOW);
-  digitalWrite(VAP1_SEL, LOW);
-  digitalWrite(VAP2, LOW);
-  digitalWrite(VAP2_SEL, LOW);
-  digitalWrite(PUMP, LOW);
-  digitalWrite(VS, LOW);
+  //inicialização dos pinos de saída, estagio 0(HIGH= rele desligado)
+  digitalWrite(VAP1, HIGH);
+  digitalWrite(VAP1_SEL, HIGH);
+  digitalWrite(VAP2, HIGH);
+  digitalWrite(VAP2_SEL, HIGH);
+  digitalWrite(PUMP, HIGH);
+  digitalWrite(VS, HIGH);
 
   Serial.begin(9600);  //inicialização da comunicação serial
   logger("Reator iniciado");
@@ -80,24 +90,24 @@ void loop() {
     case 0:  // fase de enchimento
       if (time == 0) {
         digitalWrite(VAP1_SEL, LOW);  // seleciona abertura da valvula
-        digitalWrite(VAP1, HIGH);     // aciona a valvula
+        digitalWrite(VAP1, LOW);      // aciona a valvula
         logger("Abrindo a valvula de entrada");
-        delay(TIME_VAP1);         // tempo de espera do acionamento da valvula
-        digitalWrite(VAP1, LOW);  // desliga a valvula
+        delay(TIME_VAP1);          // tempo de espera do acionamento da valvula
+        digitalWrite(VAP1, HIGH);  // desliga a valvula
         logger("valvuva de entrada aberta");
-        digitalWrite(PUMP, HIGH);  // aciona a bomba
+        digitalWrite(PUMP, LOW);  // aciona a bomba
         logger("Acionando a bomba de entrada");
         time = millis();  //inicia a contagem do tempo
         logger("Iniciando o processo de enchimento do reator");
       } else {
         if (millis() - time >= TIME_STAGE_0) {
-          digitalWrite(PUMP, LOW);  // desliga a bomba
+          digitalWrite(PUMP, HIGH);  // desliga a bomba
           logger("Bomba de entrada desligada");
           digitalWrite(VAP1_SEL, HIGH);  // seleciona fechamento da valvula
-          digitalWrite(VAP1, HIGH);      // aciona a valvula
+          digitalWrite(VAP1, LOW);       // aciona a valvula
           logger("Fechando a valvula de entrada");
-          delay(TIME_VAP1);         // tempo de espera do acionamento da valvula
-          digitalWrite(VAP1, LOW);  // desliga a valvula
+          delay(TIME_VAP1);          // tempo de espera do acionamento da valvula
+          digitalWrite(VAP1, HIGH);  // desliga a valvula
           logger("valvuva de entrada fechada");
           logger("Processo de enchimento do reator concluido");
           stage = 1;
@@ -121,11 +131,11 @@ void loop() {
       if (time == 0) {
         time = millis();  //inicia a contagem do tempo
         logger("Iniciando o processo de reação");
-        digitalWrite(VS, HIGH);  // aciona a valvula de entrada de ar comprimido
+        digitalWrite(VS, LOW);  // aciona a valvula de entrada de ar comprimido
         logger("Acionando a valvula de entrada de ar comprimido");
       } else {
         if (millis() - time >= TIME_STAGE_2) {
-          digitalWrite(VS, LOW);  // desliga a valvula de entrada de ar comprimido
+          digitalWrite(VS, HIGH);  // desliga a valvula de entrada de ar comprimido
           logger("Processo de reação concluido");
           stage = 3;
           time = 0;
@@ -149,19 +159,19 @@ void loop() {
         time = millis();  //inicia a contagem do tempo
         logger("Iniciando o processo de esvaziamento");
         digitalWrite(VAP2_SEL, LOW);  // seleciona abertura da valvula
-        digitalWrite(VAP2, HIGH);     // aciona a valvula
+        digitalWrite(VAP2, LOW);      // aciona a valvula
         logger("Abrindo a valvula de saída");
-        delay(TIME_VAP2);         // tempo de espera do acionamento da valvula
-        digitalWrite(VAP2, LOW);  // desliga a valvula
+        delay(TIME_VAP2);          // tempo de espera do acionamento da valvula
+        digitalWrite(VAP2, HIGH);  // desliga a valvula
         logger("valvuva de saída aberta");
         time = millis();  //inicia a contagem do tempo
       } else {
         if (millis() - time >= TIME_STAGE_4) {
           digitalWrite(VAP2_SEL, HIGH);  // seleciona fechamento da valvula
-          digitalWrite(VAP2, HIGH);      // aciona a valvula
+          digitalWrite(VAP2, LOW);       // aciona a valvula
           logger("Fechando a valvula de saída");
-          delay(TIME_VAP2);         // tempo de espera do acionamento da valvula
-          digitalWrite(VAP2, LOW);  // desliga a valvula
+          delay(TIME_VAP2);          // tempo de espera do acionamento da valvula
+          digitalWrite(VAP2, HIGH);  // desliga a valvula
           logger("valvuva de saída fechada");
           logger("Processo de esvaziamento concluido");
           stage = 5;
